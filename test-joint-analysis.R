37f852d0977c6142b6f5eb3626fc091246c3d3d1
Jacqueline Buros Novik <jackinovik@gmail.com>
2016-05-23 13:33:31 -0400

    update joint-model-tests to reflect changes in simulate-data & related functions

--- test-joint-analysis.R
+++ test-joint-analysis.R
@@ -8,74 +8,20 @@ options(mc.cores = min(parallel::detectCores(),3))
 ## ------ prep simulated data -------
 
 source('simulate-data.function.R')
+source('prep-data.function.R')
+source('make-data-plots.function.R')
 
 ## get simulated data 
-data <- simulate_data(n = 100, max_size = 4000, max_t = 50)
-pl <- plot_simulated_data(data, n = 12, progress_threshold = 2)
-ggsave(pl, file = 'simulated_data.png')
+data <- simulate_data(n = 100, max_size = 4000, max_t = 50, failure_threshold = 4, progression_threshold = 3)
 
-## distribution of survival times
-pl <- ggplot(data %>% 
-         dplyr::filter(observed == 1) %>%
-         group_by(patid) %>% 
-         dplyr::summarise(surv_time  = max(t, na.rm = T)
-                          , event = factor(last(failure_event, order_by = t), levels = c(0,1), labels = c('censor', 'failure'))
-                          ) %>%
-         ungroup()
-       , aes(x = surv_time, group = event, fill = event, colour = event)) +
-  geom_histogram(alpha = 0.2) +
-  facet_wrap(~event)
-ggsave(pl, file = 'distribution of event times.png')
+## review graphics for simulated data
+make_data_plots(data)
 
-## summarize inputs to survival analysis (using MLE)
-survd <- data %>%
-  dplyr::filter(observed == 1) %>%
-  dplyr::group_by(patid) %>%
-  dplyr::mutate(
-    surv_time  = max(t, na.rm = T)
-    , init_observed_size = first(observed_size, order_by = t)
-    , last_observed_size = last(observed_size, order_by = t)
-    , outcome = last(failure_event, order_by = t)
-  ) %>%
-  dplyr::filter(t == surv_time) %>%
-  ungroup() %>%
-  dplyr::mutate(avg_growth_rate = (last_observed_size - init_observed_size)/t
-                , mean_growth_rate = mean(avg_growth_rate)
-                , sd_growth_rate = sd(avg_growth_rate)
-                , rescaled_growth_rate = (avg_growth_rate - mean_growth_rate)/sd_growth_rate
-                , mean_init_size = mean(init_observed_size)
-                , sd_init_size = sd(init_observed_size)
-                , rescaled_init_size = (init_observed_size - mean_init_size)/sd_init_size
-                ) %>%
-  dplyr::select(patid, t, init_observed_size, last_observed_size, avg_growth_rate, outcome, starts_with('rescaled'))  %>%
-  dplyr::rename(last_t = t)
-
-survd %>%
-  group_by(outcome) %>%
-  tally()
-
-## create joint growth & survival model 
-newdata <- data %>%
-  inner_join(survd 
-             , by = "patid") %>%
-  filter(observed == 1) %>%
-  mutate(overall_mean_size = mean(observed_size, na.rm = T)
-         , overall_sd_size = sd(observed_size, na.rm = T)
-         ) %>%
-  group_by(patid) %>%
-  mutate(rescaled_tumor_size = (observed_size - overall_mean_size)/overall_sd_size
-         , patient_mean_size = mean(observed_size)
-         , patient_sd_size = sd(observed_size)
-         , rescaled_patient_observed_size = (observed_size - patient_mean_size)/ patient_sd_size
-  ) %>%
-  ungroup()
-  
-
-## if only one obs in censor or failure group, revisit data 
-## Stan code below will be problematic
-if (nrow(survd %>% filter(outcome == 1))==0 || nrow(survd %>% filter(outcome == 0))==0) {
-  stop('Regenerate simulated data - Stan code below will not work (yet) without some obs both censored & observed.')
-}
+## prep data for analysis
+res <- prep_data(data) 
+survd <- res$per_patient ## summarized per patient; appropriate for typical survival analysis
+adata <- res$per_observation ## denormalized; appropriate for longitudinal analysis
+rm(res)
 
 ## ------ try fitting using JM (skipped) -------
 
@@ -113,13 +59,15 @@ if (TRUE == FALSE) {
   anova(fitJOINT.null, fitJOINT.alt)
 } 
 
+## TODO try SemiCompRisks (https://cran.r-project.org/web/packages/SemiCompRisks/SemiCompRisks.pdf)
+
 ## ------ test survival model using coxph -------
 
 ## for sanity, start with separate surv + longitudinal models.
 
 library(survival)
 #aareg(formula = Surv(t, outcome) ~ init_observed_size, data = survd, nmin = 1)
-survfit <- coxph(formula = Surv(last_t, outcome) ~ rescaled_init_size, data = survd)
+survfit <- coxph(formula = Surv(first_failure, failure_status) ~ rescaled_init_size, data = survd)
 print(survfit)
 
 ## ------ test growth model using lmer -------
@@ -127,22 +75,22 @@ print(survfit)
 library(lme4)
 library(arm)
 growthfit <- lmer(rescaled_patient_observed_size ~ t + rescaled_init_size + 
-                    (1 + rescaled_init_size + t | patid)
-                  , data = newdata
+                    (1 + t || patid)
+                  , data = adata
                   )
 display(growthfit)        
 
 ## ------ test survival model (long version) using stan -------
 
 standata <- list(
-  N = nrow(newdata)
-  , S = max(newdata$patid)
-  , T = max(newdata$t)
+  N = nrow(adata)
+  , S = max(adata$patid)
+  , T = max(adata$t)
   , X = 1
-  , s = newdata$patid
-  , t = newdata$t
-  , event = newdata$failure_event
-  , covars = newdata %>% dplyr::select(rescaled_init_size)
+  , s = adata$patid
+  , t = adata$t
+  , event = adata$failure_status
+  , covars = adata %>% dplyr::select(rescaled_init_size)
 )
 
 testfit <- stan('long_surv.stan', data = standata, chains = 1, iter = 10)
@@ -158,26 +106,17 @@ print(stanfit, 'beta')
 ## using parameterization here: http://www.ncbi.nlm.nih.gov/pmc/articles/PMC4744123/ 
 ## this allows for post-progression risk to be different from pre-progression risk
 
-newdata2 <- newdata %>%
-  dplyr::group_by(patid) %>%
-  dplyr::arrange(t) %>%
-  dplyr::mutate(progression = ifelse(failure > 0, 1, 0)
-                , cum_progression = cumsum(progression)
-                , pr_progression = c(0, cum_progression[-n()]) ## take previous obs of cum_progression for current timepoint
-                ) %>%
-  ungroup()
-  
 standata2 <- list(
-  N = nrow(newdata2)
-  , S = max(newdata2$patid)
-  , T = max(newdata2$t)
+  N = nrow(adata)
+  , S = max(adata$patid)
+  , T = max(adata$t)
   , X = 1
-  , s = newdata2$patid
-  , t = newdata2$t
-  , event = newdata2$failure_event
-  , covars = newdata2 %>% dplyr::select(rescaled_init_size)
-  , progression = newdata2$progression
-  , pr_progress = newdata2$pr_progression
+  , s = adata$patid
+  , t = adata$t
+  , event = adata$failure_status
+  , covars = adata %>% dplyr::select(rescaled_init_size)
+  , progression = adata$progression
+  , pr_progress = adata$pr_progression
 )
 
 testfit2 <- stan('long_surv_progression.stan'
@@ -213,16 +152,16 @@ print(stanfit2a, c('beta_shared', 'beta1', 'beta2', 'beta3'))
 
 
 standata3 <- list(
-  N = nrow(newdata2)
-  , S = max(newdata2$patid)
-  , T = max(newdata2$t)
+  N = nrow(adata)
+  , S = max(adata$patid)
+  , T = max(adata$t)
   , X = 1
-  , s = newdata2$patid
-  , t = newdata2$t
-  , event = newdata2$failure_event
-  , covars = newdata2 %>% dplyr::select(rescaled_init_size)
-  , progression = newdata2$progression
-  , pr_progress = newdata2$pr_progression
+  , s = adata$patid
+  , t = adata$t
+  , event = adata$failure_status
+  , covars = adata %>% dplyr::select(rescaled_init_size)
+  , progression = adata$progression
+  , pr_progress = adata$pr_progression
 )
 
 testfit3 <- stan('long_surv_progression2.stan'
@@ -242,16 +181,16 @@ print(stanfit3, c('beta'))
 ## variation of model allowing betas to be different
 
 standata4 <- list(
-  N = nrow(newdata2)
-  , S = max(newdata2$patid)
-  , T = max(newdata2$t)
+  N = nrow(adata)
+  , S = max(adata$patid)
+  , T = max(adata$t)
   , X = 1
-  , s = newdata2$patid
-  , t = newdata2$t
-  , event = newdata2$failure_event
-  , covars = newdata2 %>% dplyr::select(rescaled_init_size)
-  , progression = newdata2$progression
-  , pr_progress = newdata2$pr_progression
+  , s = adata$patid
+  , t = adata$t
+  , event = adata$failure_status
+  , covars = adata %>% dplyr::select(rescaled_init_size)
+  , progression = adata$progression
+  , pr_progress = adata$pr_progression
 )
 
 testfit4 <- stan('long_surv_progression2_free_beta.stan'
